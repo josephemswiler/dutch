11e22eb1f50bf36b2d750903314a4e18914bb25f
josephemswiler <josephemswiler@gmail.com>
2018-07-22 19:32:02 -0500

    init card, gradients, fonts

--- app/App.js
+++ app/App.js
@@ -7,28 +7,35 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, StatusBar} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {StyleSheet, Text, View} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient'
+import { Fonts } from './src/utils/Fonts'
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-      <View style={styles.statusGutter} />
+      
+      <LinearGradient style={styles.container} colors={['#fab1a0', '#ff3f34']}>
+        <View style={styles.statusGutter} />
         <View style={styles.title}>
           <Text style={styles.titleText}>dutch</Text>
         </View>
-        <View style={styles.content}>
-          <Text>Split expenses amongst friends.</Text>
-        </View>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+          <View style={styles.content}>
+            <View style={styles.card}>
+              <View style={styles.upperCard}>
+                <Text style={styles.cardTitle}>Dinner at Torchy's</Text>
+                <Text style={styles.cardDetails}>Torchy's Tacos</Text>
+                <Text style={styles.cardDetails}>06/27/2018</Text>
+              </View>
+              <View style={styles.lowerCard}>
+              <Text style={styles.cardFooterLeft}>You received</Text>
+              <Text style={styles.cardFooterRight}>$140.00</Text>
+              </View>
+            </View>
+            
+          </View>
+      </LinearGradient>
+      
     );
   }
 }
@@ -37,13 +44,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-   
+    alignItems: 'center'
   },
   statusGutter: {
     height: 20,
-    backgroundColor: 'rgba( 0, 0, 0, 0.8)',
+    backgroundColor: 'rgba( 255, 255, 255, 0.8)',
     width: '100%',
   },
   title: {
@@ -60,9 +65,53 @@ const styles = StyleSheet.create({
   content: {
     flex: 2,
     textAlign: 'center',
-    color: '#333333',
-    backgroundColor: 'red',
-    height: '100%',
-    width: '100%'
+    width: '100%',
+    padding: 20,
   },
+  card: {
+    height: 100,
+    textAlign: 'center',
+    color: '#333333',
+    backgroundColor: 'rgba( 255, 255, 255, 0.8)',
+    borderRadius: 5,
+  },
+  upperCard: {
+    padding: 15
+  },
+  cardTitle: {
+    fontSize: 20,
+    fontFamily: Fonts.WorkSansRegular
+  },
+  cardDetails: {
+    fontSize: 12,
+    fontFamily: Fonts.WorkSansRegular,
+    marginTop: 4
+  },
+  lowerCard: {
+    backgroundColor: '#485460',
+    padding: 10,
+    width: '100%',
+    borderRadius: 5,
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cardFooterLeft: {
+    color: '#ffffff',
+    paddingLeft: 5,
+    fontSize: 12,
+    fontFamily: Fonts.WorkSansRegular,
+    
+
+  },
+  cardFooterRight: {
+    color: '#0BE881',
+    textAlign: 'right',
+    paddingRight: 5,
+    fontSize: 20,
+    fontFamily: Fonts.WorkSansBold,
+    marginLeft: 'auto',
+  }
 });
